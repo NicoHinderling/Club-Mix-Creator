4aea72ae99ec0708f01ceda782ff62f8ef726c30
NicoHinderling <nicolas.hinderling@gmail.com>
2016-03-23 19:43:13 -0400

    prototype api functioning

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-var express = require('express');
-var app = express();
+require('dotenv').config();
 
-// http://developer.echonest.com/api/v4/
+var express  = require('express')
+  , echojs = require('echojs')
+  , app      = express()
+  , mongoose = require('mongoose')
+  , passport = require('passport')
+  , flash    = require('connect-flash')
+  , morgan       = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser   = require('body-parser')
+  , session      = require('express-session')
+  // , configDB = require('./config/database.js')
+  , port     = process.env.PORT || 3000
+  , echo = echojs({ key: process.env.ECHONEST_KEY });
 
-app.get('/', function (req, res) {
-  routes = ["/", "/login", "/lookup"]
-  var homepageText = 'Welcome to Club-Mix Creator. Available routes:'
-  for (i = 0; i < routes.length; i++) { 
-    homepageText += "\n- '" + routes[i] + "'";
-  }
-  res.send();
-});
 
-app.get('/login', function (req, res) {
-  res.send('Login');
-});
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
-app.get('/lookup', function (req, res) {
-  res.send('lookup');
-});
+// mongoose.connect(configDB.url);
+require('./config/passport')(passport);
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Club DJ listening on port 3000!');
-});
+//Passport
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//Routes
+require('./routes.js')(echo, app, passport);
+
+app.listen(port);
+console.log('Now running on port: ' + port);
---
+++ routes.js
@@ -0,0 +1,90 @@
+// http://developer.echonest.com/api/v4/
+
+module.exports = function(echo, app, passport) {
+
+    app.get('/', function(req, res) {
+      var homepageText = "Welcome to Club-Mix Creator. Available routes: ['/', '/login', '/lookup']"
+      res.render('pages/index.ejs', { message: homepageText });
+    });
+
+    app.get('/lookup', function (req, res) {
+      res.render('pages/lookup.ejs', {message: ""}); 
+    });
+
+    app.post('/lookup', function(req, res) {
+      console.log(req.body.title);
+      console.log(req.body.artist);
+      if (req.body.artist === '' || req.body.title === '') {
+        console.log("broke");
+        res.render('pages/lookup.ejs', {message: "Please include both a song name and title"});
+      }
+      else {
+        lookupParams = {title: req.body.title, artist: req.body.artist}
+        echo('song/search').get(lookupParams, function (err, json) {
+          res.render('pages/results.ejs', { results: json.response['songs']}); 
+        });
+      }
+    });
+
+    app.get('/songSearch/:id', function(req, res) {
+      id = req.params.id
+      echo('song/profile').get({
+        id: id,
+        bucket: 'audio_summary'
+      }, function (err, json) {
+        audio_summary = json.response['songs'][0]['audio_summary'];
+        console.log(audio_summary); 
+        res.send(audio_summary);
+      });
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile',
+        failureRedirect : '/login',
+        failureFlash : true
+    }));
+
+    app.get('/login', function(req, res) {
+        res.render('pages/login.ejs', { message: req.flash('loginMessage') }); 
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile',
+        failureRedirect : '/login',
+        failureFlash : true
+    }));
+
+    app.get('/signup', function(req, res) {
+        res.render('pages/signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile',
+        failureRedirect : '/signup',
+        failureFlash : true
+    }));
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('pages/profile.ejs', {
+            user : req.user
+        });
+    });
+
+     app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/500', function(req, res){
+        console.log("500 hit");
+        throw new Error('This is a 500 Error');
+    });
+
+};
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
